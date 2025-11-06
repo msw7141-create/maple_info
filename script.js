090e7cb9c6b9d43db9bea933be44e00e8470dbee
msw7141-create <msw7141@gmail.com>
2025-11-06 13:37:36 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+async function searchCharacter(){
+    const nickname= document.getElementById("nickname").value;
+
+    const resultDiv= document.getElementById("result");
+    resultDiv.innerText =nickname+"검색중..."
+
+    try {
+        const response= await fetch("https://open.api.nexon.com/maplestory/v1/id", {headers :{
+             "x-nxopen-api-key": "fd8eed0087767b0ff3fee45805afa966e3ac740204edfea96c222a3f9a392e79efe8d04e6d233bd35cf2fabdeb93fb0d"
+        }})
+    
+        const data = await response.json();
+        resultDiv.innerHTML = `<p>캐릭터 이름: ${data.character_name} </p>`;
+    }
+    catch(error){
+        resultDiv.innerText="에러가 발생하였습니다."
+        console.error(error)
+    }
+}
+
+
+document.getElementById("search").addEventListener("click",searchCharacter)
